a6016943cd1a10ea94962b1f6a25da03299ea77a
shank <220701306@rajalakshmi.edu.in>
2025-09-22 01:47:39 +0530

    initial backend with user auth

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# backend/Dockerfile
+FROM node:24-alpine
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+EXPOSE 5000
+
+CMD ["npm", "run", "dev"]
---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+
+import { connectDB } from "./libs/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(
+    "Server is running on http://localhost:" + PORT
+  );
+  connectDB();
+});
