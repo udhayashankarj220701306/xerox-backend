fbf279ff049209bdf6daa7007b8538d1ba1b7f15
shank <220701306@rajalakshmi.edu.in>
2025-09-24 03:21:10 +0530

    feat:model created

---
+++ models/xerox.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const xeroxSchema = new mongoose.Schema({
+  xeroxId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "shopid is required"],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  colorOption: [
+    {
+      type: String,
+      enum: ["bw", "color"],
+    },
+  ],
+  paperOption: [
+    {
+      type: String,
+      enum: ["A4", "A3", "A5"],
+    },
+  ],
+  layoutOption: [
+    {
+      type: String,
+      enum: ["potrait", "landscape"],
+    },
+  ],
+  sidesOption: [
+    {
+      type: String,
+      enum: ["single", "double"],
+    },
+  ],
+  bindingOption: [
+    {
+      type: String,
+      enum: ["soft", "spiral"],
+    },
+  ],
+});
+
+const Xerox = mongoose.model("Xerox", xeroxSchema);
+export default Xerox;
