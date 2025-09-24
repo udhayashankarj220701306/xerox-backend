e6c7499a170321231da7cb99852cabea84284857
shank <220701306@rajalakshmi.edu.in>
2025-09-25 01:21:55 +0530

    feat:added xerox profile

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,49 +1,63 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
+import Xerox from "../models/xerox.model.js";
 
 export const protectRoute = async (req, res, next) => {
-	try {
-		const accessToken = req.cookies.accessToken;
+  try {
+    const accessToken = req.cookies.accessToken;
 
-		if (!accessToken) {
-			return res.status(401).json({ message: "Unauthorized - No access token provided" });
-		}
+    if (!accessToken) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No access token provided" });
+    }
 
-		try {
-			const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-			const user = await User.findById(decoded.userId).select("-password");
+    try {
+      const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+      const user = await User.findById(decoded.userId).select("-password");
 
-			if (!user) {
-				return res.status(401).json({ message: "User not found" });
-			}
+      if (!user) {
+        return res.status(401).json({ message: "User not found" });
+      }
+      req.user = user;
+      if (user.role === "xerox") {
+        const xeroxProfile = await Xerox.findOne({ xeroxId: user._id });
+        if (req.user.toObject) {
+          req.user = req.user.toObject(); // Convert it to a plain object
+        }
+        req.user.xeroxProfile = xeroxProfile;
+        console.log("Fetching xerox profile for user", req.user);
+      }
 
-			req.user = user;
-
-			next();
-		} catch (error) {
-			if (error.name === "TokenExpiredError") {
-				return res.status(401).json({ message: "Unauthorized - Access token expired" });
-			}
-			throw error;
-		}
-	} catch (error) {
-		console.log("Error in protectRoute middleware", error.message);
-		return res.status(401).json({ message: "Unauthorized - Invalid access token" });
-	}
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - Access token expired" });
+      }
+      throw error;
+    }
+  } catch (error) {
+    console.log("Error in protectRoute middleware", error.message);
+    return res
+      .status(401)
+      .json({ message: "Error in protectRoute middleware" });
+  }
 };
 
 export const adminRoute = (req, res, next) => {
-	if (req.user && req.user.role === "admin") {
-		next();
-	} else {
-		return res.status(403).json({ message: "Access denied - Admin only" });
-	}
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res.status(403).json({ message: "Access denied - Admin only" });
+  }
 };
 
 export const xeroxRoute = (req, res, next) => {
-	if (req.user && req.user.role === "xerox") {
-		next();
-	} else {
-		return res.status(403).json({ message: "Access denied - Xerox only" });
-	}
+  if (req.user && req.user.role === "xerox") {
+    next();
+  } else {
+    return res.status(403).json({ message: "Access denied - Xerox only" });
+  }
 };
