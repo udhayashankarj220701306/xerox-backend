e1585e2928d5ad35234703c48eb388bc9ba9069e
Shiny A <220701267@rajalakshmi.edu.in>
2025-09-24 11:29:02 +0530

    feat:xerox server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
+import xeroxRoutes from "./routes/xerox.route.js";
 
 import { connectDB } from "./libs/db.js";
 
@@ -17,6 +18,7 @@ app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/xerox", xeroxRoutes);
 
 app.listen(PORT, () => {
   console.log(
