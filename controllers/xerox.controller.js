96422f91ea26d9dd87ca18f2fe65e06dd560e1a9
Shiny A <220701267@rajalakshmi.edu.in>
2025-09-24 10:47:46 +0530

    feat/get and update xerox profile

--- controllers/xerox.controller.js
+++ controllers/xerox.controller.js
@@ -1,2 +1,34 @@
 import Xerox from "../models/xerox.model.js";
 
+export const getProfile = async (req, res) => {
+    try {
+        const xeroxId = req.params.id;
+        const xeroxProfile = await Xerox.findOne({ xeroxId });
+        if (!xeroxProfile) {
+            return res.status(404).json({ message: "Xerox profile not found" });
+        }
+        else {
+            res.status(200).json(xeroxProfile);
+        }
+    } catch (error) {
+        console.log("Error in getProfile controller", error.message);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+        const xeroxId = req.params.id;
+        const updates = req.body;
+        const xeroxProfile = await Xerox.findOneAndUpdate({ xeroxId }, updates, { new: true });
+        if (!xeroxProfile) {
+            return res.status(404).json({ message: "Xerox profile not found" });
+        }
+        else {
+            res.status(200).json(xeroxProfile);
+        }
+    } catch (error) {
+        console.log("Error in updateProfile controller", error.message);
+        res.status(500).json({ message: error.message });
+    }
+}
