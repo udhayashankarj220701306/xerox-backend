fa37fe3737c9aa9e6c3e86e99d3a878532c57fca
shank <220701306@rajalakshmi.edu.in>
2025-10-09 23:31:55 +0530

    feat:local dev and vercel prod

--- server.js
+++ server.js
@@ -10,18 +10,19 @@ import { connectDB } from "./libs/db.js";
 
 dotenv.config();
 
+connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
-    credentials: true, // This is the key change
+    credentials: true, 
     optionsSuccessStatus: 200,
   })
 );
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+app.use(express.json({ limit: "10mb" })); 
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -30,12 +31,12 @@ app.use("/api/request", requestRoutes);
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+if (process.env.NODE_ENV === "development") {
+  app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:" + PORT);
+  });
+}
 
-// app.listen(PORT, () => {
-//   console.log(
-//     "Server is running on http://localhost:" + PORT
-//   );
-  connectDB();
-// });
+
 
 export  default app;
